a731c423ad9506b22c6901e33d6ea8b6362f2444
nikitasA1NT <14nikita14m@gmail.com>
2020-11-11 00:53:00 +0400

    Made TreeNode struct - element of binary tree

---
+++ BinaryTree.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Binary Tree
+class BinaryTree
+{
+	// Element of a binary tree
+	struct TreeNode
+	{
+		int info;	// Information part
+		TreeNode* pLeft, * pRight;	// Left & right branches
+	};
+
+
+};
