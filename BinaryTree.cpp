a5c14e5ea38633a449174637ef23cf1a66734ee9
nikitasA1NT <14nikita14m@gmail.com>
2020-11-17 00:54:33 +0400

    create BinaryTree class & TreeNode struct inside him & addElement method

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,1 +1,17 @@
 #include "BinaryTree.h"
+
+void BinaryTree::addElement(int n)
+{
+	r_addElement(root, n);
+}
+
+void BinaryTree::r_addElement(TreeNode* node, int info)
+{
+	if (!node)	// if current node is nullptr
+		node = new TreeNode(info);	//	add element
+	else
+		if (info < node->info)
+			r_addElement(node->pLeft, info);
+		else // if (info >= node->info)
+			r_addElement(node->pRight, info);
+}
--- BinaryTree.h
+++ BinaryTree.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Binary Tree
+// Binary Search Tree
 class BinaryTree
 {
 	// Element of a binary tree
@@ -8,7 +8,29 @@ class BinaryTree
 	{
 		int info;	// Information part
 		TreeNode* pLeft, * pRight;	// Left & right branches
+
+		TreeNode(int info, TreeNode* pLeft = nullptr, TreeNode* pRight = nullptr)
+		{
+			this->info = info;
+			this->pLeft = pLeft;
+			this->pRight = pRight;
+		}
 	};
 
 
+	TreeNode* root;	// Root of a tree
+
+public:
+
+	// Constructors & destructor
+	BinaryTree() { this->root = nullptr; }
+
+	// Add element
+	void addElement(int n);
+
+
+private:	// "r_" - recursive private methods
+
+	// Add element (private)
+	void r_addElement(TreeNode* node, int n);
 };
